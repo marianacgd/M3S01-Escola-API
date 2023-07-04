fea049372459e16092d19b53f64bff96915cb38f
Vitor Lassen <vitor@lassen.com.br>
2023-07-04 20:12:34 -0300

    feat : create table

---
+++ Escola.API/DataBase/EscolaDbContexto.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Escola.API.Model;
+
+namespace Escola.API.DataBase
+{
+    public class EscolaDbContexto : DbContext
+    {
+        public virtual DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Password=P@ssword;Persist Security Info=True;User ID=sa;Initial Catalog=EscolaDB-Audaces;Data Source=tcp:localhost,1433");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aluno>().ToTable("AlunoTB");
+
+            modelBuilder.Entity<Aluno>().HasKey(x => x.Id).HasName("Pk_aluno_id");
+
+            modelBuilder.Entity<Aluno>().Property(x => x.Nome)
+                                        .IsRequired()
+                                        .HasMaxLength(50);
+
+            modelBuilder.Entity<Aluno>().Property(x => x.Email)
+                                        .IsRequired()
+                                        .HasColumnType("VARCHAR");
+
+        }
+    }
+}
---
+++ Escola.API/Model/Aluno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Escola.API.Model
+{
+    public class Aluno
+    {        
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public int Idade { get; set; }
+        public string Genero { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
